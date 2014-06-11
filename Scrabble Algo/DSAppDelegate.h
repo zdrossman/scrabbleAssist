ab9a27bd11fa059b897ac991fa1770f953c746a6
danielwmsun <dan@waadle.com>
2014-06-11 15:38:11 -0400

    Initial Commit

---
+++ Scrabble Algo/DSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DSAppDelegate.h
+//  Scrabble Algo
+//
+//  Created by Daniel Sun on 6/11/14.
+//  Copyright (c) 2014 Daniel Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Scrabble Algo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Scrabble Algo
+//
+//  Created by Daniel Sun on 6/11/14.
+//  Copyright (c) 2014 Daniel Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DSAppDelegate class]));
+    }
+}
